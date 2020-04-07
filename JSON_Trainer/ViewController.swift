9bde21a15d9f538be255afe8b9451272643e3385
Zinko Viacheslav <zslavman@gmail.com>
2020-04-07 22:59:29 +0300

    Initial Commit

---
+++ JSON_Trainer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSON_Trainer
+//
+//  Created by Zinko Viacheslav on 07.04.2020.
+//  Copyright © 2020 Zinko Viacheslav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
